81ac83d30aa046a936c22236d6acee78f2103b10
Ravi Joshi <ravipiyushjoshi@gmail.com>
2020-07-13 14:17:31 -0400

    Changed with few components

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Image, StyleSheet, Text, View, Button } from 'react-native';
+import logo from './assets/logo.jpg';
 
 export default function App() {
+  const [outputText,setOutputText] = useState('Awesome App tunred into Hello World');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{width:300, height:159}}/>
+      <Text style={{color: '#888', fontSize: 18}}>
+       {outputText}
+      </Text>
+      <Button
+             title="ChangeText"
+             onPress={()=>setOutputText('Hello World is changed')} 
+              />
       <StatusBar style="auto" />
     </View>
   );
